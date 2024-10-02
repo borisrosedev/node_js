ed476aafa4a2d2ca2af9cc3633b800a460686638
borisrose <boris.alexandre.rose@gmail.com>
2024-10-03 01:22:44 +0200

    feat/clean/v1

--- helpers/tryCatch.helper.js
+++
@@ -1,8 +0,0 @@
-export default async (cb, res) => {
-  try {
-    return await cb();
-  } catch (error) {
-    res.writeHead(500, { "Content-Type": "text/plain" });
-    res.write("Error reading file");
-  }
-};
